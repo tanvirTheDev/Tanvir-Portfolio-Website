026ddbbc3362e591f634a015dde9efd9013bb735
Tanvir Ahamed <ahamedtanvir374@gmail.com>
2025-04-19 16:31:43 +0600

    change image design

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,62 +1,30 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import myImage from "../../public/assets/myImage.png";
+import myImage from "../../public/assets/myImage.jpg";
+
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
-      {/* image */}
+    <div className="relative w-full h-full flex items-center justify-center">
+      {/* Animated Circle SVG in background */}
+
+      {/* Box-framed image (not circular) */}
       <motion.div
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { delay: 2, duration: 0.4, ease: "easeIn" },
+          transition: { delay: 2.4, duration: 0.6, ease: "easeInOut" },
         }}
+        className="relative w-[280px] h-[280px] xl:w-[440px] xl:h-[440px] border-[6px] border-[#00ff99] z-10"
       >
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{
-            opacity: 1,
-            transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
-          }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute left-0 xl:left-3"
-        >
-          <Image
-            src={myImage}
-            alt="photo"
-            priority
-            quality={100}
-            fill
-            className="object-contain"
-          />
-        </motion.div>
-        {/* circle */}
-        <motion.svg
-          className="w-[300px] xl:w-[586px] h-[300px] xl:h-[506px]"
-          fill="transparent"
-          viewBox="0 0 506 506"
-          xmlns="http://www.org/2000/svg"
-        >
-          <motion.circle
-            cx="253"
-            cy="253"
-            r="250"
-            stroke="#00ff99"
-            strokeWidth="4"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            initial={{ strokeDasharray: "24 10 0 0" }}
-            animate={{
-              strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-              rotate: [120, 360],
-            }}
-            transition={{
-              duration: 20,
-              repeat: Infinity,
-              repeatType: "reverse",
-            }}
-          />
-        </motion.svg>
+        <Image
+          src={myImage}
+          alt="Profile"
+          fill
+          className="object-cover"
+          priority
+          quality={100}
+        />
       </motion.div>
     </div>
   );
